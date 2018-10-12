ba43c652c1dc769a5d18f1e692434190cb116203
Administrator <903857227@qq.com>
2018-10-12 14:54:59 +0800

    添加编辑功能

--- src/main/java/com/eill/llg/LlgWebSpringbootClassApplication.java
+++ src/main/java/com/eill/llg/LlgWebSpringbootClassApplication.java
@@ -1,5 +1,6 @@
 package com.eill.llg;
 
+import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LlgWebSpringbootClassApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(LlgWebSpringbootClassApplication.class, args);
     }
 }
